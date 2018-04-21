cdd7543ce94fcb974127179f3800667de8dab44b
Joe Ortega <joeortega79@gmail.com>
2018-04-21 13:39:51 +0930

    bird physics and mechanics implemented.

--- core/src/com/sbsgames/flappybird/FlappyBird.java
+++ core/src/com/sbsgames/flappybird/FlappyBird.java
@@ -11,7 +11,11 @@ public class FlappyBird extends ApplicationAdapter {
 	private Texture[] birds;
 	private Texture[] tubes;
 
-	private int flapState = 0;
+	private int flapState = 0, flapCount = 0;
+	private final int FLAP_TIME = 500, GRAVITY = 2;
+	private float birdY, velocity = 0;
+
+	private int gameState = 0;
 	
 	@Override
 	public void create () {
@@ -20,6 +24,7 @@ public class FlappyBird extends ApplicationAdapter {
 		birds = new Texture[2];
 		birds[0] = new Texture("bird.png");
 		birds[1] = new Texture("bird2.png");
+		birdY = (Gdx.graphics.getHeight() / 2) - (birds[0].getHeight() / 2);
 
 		tubes = new Texture[2];
 		tubes[0] = new Texture("toptube.png");
@@ -29,16 +34,36 @@ public class FlappyBird extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		
-		if(flapState == 0) {
-			flapState = 1;
+
+		if(gameState != 0) {
+
+			if (Gdx.input.justTouched()) {
+				velocity = -30;
+			}
+
+			if(birdY > 0 || velocity < 0) {
+				velocity += GRAVITY;
+				birdY -= velocity;
+			}
+
 		} else {
-			flapState = 0;
+			if (Gdx.input.justTouched()) {
+				gameState = 1;
+			}
+		}
+		if (flapCount < FLAP_TIME) {
+			flapCount++;
+		} else {
+			if (flapState == 0) {
+				flapState = 1;
+			} else {
+				flapState = 0;
+			}
 		}
 
 		batch.begin();
 		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());  // display background in full screen
-		batch.draw(birds[flapState], (Gdx.graphics.getWidth() / 2) - (birds[flapState].getWidth() / 2), (Gdx.graphics.getHeight() / 2) - (birds[flapState].getHeight() / 2));
+		batch.draw(birds[flapState], (Gdx.graphics.getWidth() / 2) - (birds[flapState].getWidth() / 2), birdY);
 		batch.end();
 	}
 	
